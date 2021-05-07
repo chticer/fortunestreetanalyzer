68659ec0bb2cb798bb165abd1a87d75a659fb433
Charlie Ticer <chticer@rgv.rr.com>
2021-05-07 14:25:52 -0500

    Add index page
    This adds the index page. It includes a simple styling layout and AJAX loading and calling capabilities.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fortunestreetanalyzer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/index.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function ()
+{
+    const applicationURI = $("input[name=\"__ApplicationURI\"]").val();
+
+    $("input[name=\"__ApplicationURI\"]").remove();
+
+    function ajaxCall(type, action, data)
+    {
+        return $.ajax
+        (
+            {
+                type: type,
+                url: applicationURI + "/Index?handler=" + action,
+                data: JSON.stringify(data),
+                contentType: "application/json; charset=utf-8"
+            }
+        );
+    }
+
+    function loadingDisplay()
+    {
+        return  "<div class=\"loading\">" +
+                    "<div>" +
+                        "<div>" +
+                            "<span class=\"fas fa-spinner fa-pulse fa-fw\"></span>" +
+                        "</div>" +
+
+                        "<div>" +
+                            "<h4>Loading...</h4>" +
+                        "</div>" +
+                    "</div>" +
+
+                    "<div>Please wait a few moments.</div>" +
+                "</div>";
+    }
+
+    let settingsContainer = $("#settings-panel > div:last-of-type");
+
+    settingsContainer.append(loadingDisplay());
+});
