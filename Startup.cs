3ef22d3af6f639165e50c9d612f4f98281549375
Charlie Ticer <chticer@rgv.rr.com>
2021-06-18 15:58:50 -0500

    Update database connection string
    This updates the connection string used to connect to the fortunestreet database on Azure SQL.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using fortunestreetanalyzer.DatabaseModels.fortunestreet;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,11 @@ namespace fortunestreetanalyzer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<FortuneStreetAppContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("FortuneStreetDatabase"));
+            });
 
             services.AddMvc().AddRazorPagesOptions(options =>
             {
