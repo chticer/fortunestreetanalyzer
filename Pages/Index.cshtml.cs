d83bd1a082615494f055c11168708547e9ca7f46
Charlie Ticer <chticer@rgv.rr.com>
2021-06-18 16:04:21 -0500

    Update pre-game settings
    This updates the pre-game settings to allow the user to select the rule, board, and mii color for the current analyzer instance. This will also allow saving of analyzer data at any time.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,11 +1,162 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using fortunestreetanalyzer.DatabaseModels.fortunestreet;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
 
 namespace fortunestreetanalyzer.Pages
 {
     public class IndexModel : PageModel
     {
-        public IndexModel()
+        private readonly FortuneStreetAppContext _fortuneStreetAppContext;
+
+        public class SaveAnalyzerDataModel
         {
+            public List<string> keys { get; set; }
+            public Global.AnalyzerDataModel analyzerData { get; set; }
+        }
+
+        public IndexModel(FortuneStreetAppContext fortuneStreetAppContext)
+        {
+            _fortuneStreetAppContext = fortuneStreetAppContext;
+        }
+
+        public JsonResult OnGetLoadGameSelection()
+        {
+            Global.Response response = new Global.Response();
+
+            try
+            {
+                List<Rules> rules = _fortuneStreetAppContext.Rules.OrderBy(name => name.Name).ToList();
+
+                List<Boards> boards = _fortuneStreetAppContext.Boards.OrderBy(name => name.Name).ToList();
+
+                List<Colors> miiColors = _fortuneStreetAppContext.Colors.OrderBy(id => id.ID).ToList();
+
+                response.HTMLResponse = JsonSerializer.Serialize(new
+                {
+                    Data = new Global.AnalyzerDataModel
+                    {
+                        GameSelection = new Global.AnalyzerDataModel.GameSelectionDataModel
+                        {
+                            RuleID = rules.FirstOrDefault().ID,
+                            BoardID = boards.FirstOrDefault().ID,
+                            ColorID = miiColors.FirstOrDefault().ID
+                        }
+                    },
+                    Response =
+                        "<div id=\"game-selection\">" +
+                            "<div>" +
+                                "<div>" +
+                                    "<h5>Rules</h5>" +
+                                "</div>" +
+
+                                "<div>" +
+                                    "<select class=\"form-select\">" +
+
+                                        string.Join("", rules.Select(rule => "<option value=\"" + rule.ID + "\">" + rule.Name + "</option>")) +
+
+                                    "</select>" +
+                                "</div>" +
+                            "</div>" +
+
+                            "<div>" +
+                                "<div>" +
+                                    "<h5>Boards</h5>" +
+                                "</div>" +
+
+                                "<div>" +
+                                    "<select class=\"form-select\">" +
+
+                                        string.Join("", boards.Select(board => "<option value=\"" + board.ID + "\">" + board.Name + "</option>")) +
+
+                                    "</select>" +
+                                "</div>" +
+                            "</div>" +
+
+                            "<div>" +
+                                "<div>" +
+                                    "<h5>Mii Color</h5>" +
+                                "</div>" +
+
+                                "<div class=\"color-selection center-items\">" +
+
+                                    string.Join("", miiColors.Select
+                                    (
+                                        (color, index) =>
+                                            "<div>" +
+                                                "<input type=\"hidden\" data-colorid=\"" + color.ID + "\" />" +
+
+                                                "<div style=\"background-color: #" + color.MiiColor + "\"" + (index == 0 ? " class=\"selected\"" : "") + "></div>" +
+                                            "</div>"
+                                    )) +
+
+                                "</div>" +
+                            "</div>" +
+
+                            Global.CreateConfirmationActions("center-items", new List<string>
+                            {
+                                "<button type=\"button\" class=\"btn btn-lg btn-primary\" name=\"confirm\">Confirm</button>"
+                            }) +
+
+                        "</div>"
+                });
+
+                return new JsonResult(response);
+            }
+            catch (Exception e)
+            {
+                return Global.ServerErrorResponse(e);
+            }
+        }
+
+        public JsonResult OnPostSaveAnalyzerData([FromBody] SaveAnalyzerDataModel saveAnalyzerParameter)
+        {
+            Global.Response response = new Global.Response();
+
+            try
+            {
+                long? analyzerInstanceID = Global.VerifyAnalyzerInstanceID(saveAnalyzerParameter.analyzerData.AnalyzerInstanceID, _fortuneStreetAppContext);
+
+                if (analyzerInstanceID == null)
+                    throw new Exception();
+
+                List<PropertyInfo> analyzerDataModelProperties = typeof(Global.AnalyzerDataModel).GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList();
+
+                foreach (PropertyInfo currentAnalyzerDataModelProperty in analyzerDataModelProperties)
+                {
+                    if (saveAnalyzerParameter.keys.Contains(currentAnalyzerDataModelProperty.Name))
+                        _fortuneStreetAppContext.AnalyzerInstanceLogs.Add(new AnalyzerInstanceLogs
+                        {
+                            AnalyzerInstanceID = (long) analyzerInstanceID,
+                            Key = currentAnalyzerDataModelProperty.Name,
+                            Value = JsonSerializer.Serialize(currentAnalyzerDataModelProperty.GetValue(saveAnalyzerParameter.analyzerData, null))
+                        });
+                }
+
+                _fortuneStreetAppContext.SaveChanges();
+
+                response.AlertData = new Global.Response.Alert
+                {
+                    Type = "alert-success",
+                    Title = "Saved data...",
+                    Descriptions = new List<string>()
+                };
+            }
+            catch
+            {
+                response.AlertData = new Global.Response.Alert
+                {
+                    Type = "alert-warning",
+                    Title = "Cannot save data...",
+                    Descriptions = new List<string>()
+                };
+            }
+
+            return new JsonResult(response);
         }
     }
 }
--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -4,6 +4,10 @@
 
     $("input[name=\"__ApplicationURI\"]").remove();
 
+    let analyzerData = {};
+
+    const settingsContainer = $("#settings-panel > div:last-of-type");
+
     function ajaxCall(type, action, data)
     {
         return $.ajax
@@ -15,6 +19,36 @@
                 contentType: "application/json; charset=utf-8"
             }
         );
+    }
+
+    function alertDisplay(alert)
+    {
+        if (alert !== null)
+        {
+            let descriptions = $.map(alert["Descriptions"], function (value)
+            {
+                if (value !== null)
+                    return "<div>" + value + "</div>";
+            });
+
+            if (descriptions.length > 0)
+            {
+                settingsContainer.find(".alert").remove();
+
+                settingsContainer.prepend
+                (
+                    "<div class=\"alert alert-dismissible " + alert["Type"] + "\">" +
+                        "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\"></button>" +
+
+                        "<div>" +
+                            "<strong>" + alert["Title"] + "</strong>" +
+                        "</div>" +
+
+                        "<div>" + descriptions.join("") + "</div>" +
+                    "</div>"
+                );
+            }
+        }
     }
 
     function loadingDisplay()
@@ -34,7 +68,105 @@
                 "</div>";
     }
 
-    let settingsContainer = $("#settings-panel > div:last-of-type");
+    function saveAnalyzerData(keys)
+    {
+        ajaxCall
+        (
+            "POST",
+            "SaveAnalyzerData",
+            {
+                keys: keys,
+                analyzerData: analyzerData
+            }
+        );
+
+        // TODO
+        // Call alertDisplay with "Saving to database..." alert at bottom.
+    }
+
+    function initializeDetermineCharacterSettings()
+    {
+        let gameSelectionRulesSelectContainer = $("#game-selection > div:first-of-type select");
+
+        gameSelectionRulesSelectContainer.addClass("disabled");
+        gameSelectionRulesSelectContainer.prop("disabled", true);
+
+        let gameSelectionBoardsSelectContainer = $("#game-selection > div:nth-of-type(2) select");
+
+        gameSelectionBoardsSelectContainer.addClass("disabled");
+        gameSelectionBoardsSelectContainer.prop("disabled", true);
+
+        let gameSelectionColorSelectionContainer = $("#game-selection > div:nth-of-type(3) > .color-selection");
+
+        gameSelectionColorSelectionContainer.addClass("disabled");
+        gameSelectionColorSelectionContainer.children().children().off("click");
+    }
 
     settingsContainer.append(loadingDisplay());
+
+    ajaxCall
+    (
+        "GET",
+        "LoadGameSelection",
+        {}
+    ).done(function (response)
+    {
+        settingsContainer.empty();
+
+        alertDisplay(response["AlertData"]);
+
+        if (!response["Error"])
+        {
+            let JSONResponse = JSON.parse(response["HTMLResponse"]);
+
+            analyzerData = JSONResponse["Data"];
+
+            settingsContainer.append(JSONResponse["Response"]);
+
+            $("#game-selection > div:first-of-type select").on("change", function ()
+            {
+                analyzerData["GameSelection"]["RuleID"] = Number($(this).val());
+            });
+
+            $("#game-selection > div:nth-of-type(2) select").on("change", function ()
+            {
+                analyzerData["GameSelection"]["BoardID"] = Number($(this).val());
+            });
+
+            $("#game-selection > div:nth-of-type(3) > .color-selection input[type=\"hidden\"]").each(function ()
+            {
+                let colorSelectionInputContainer = $(this);
+
+                let currentColorContainer = colorSelectionInputContainer.parent();
+
+                let colorSelectionInputData =
+                {
+                    ColorID: Number(colorSelectionInputContainer.data("colorid"))
+                };
+
+                colorSelectionInputContainer.remove();
+
+                currentColorContainer.children().first().on("click", function ()
+                {
+                    if (analyzerData["GameSelection"]["ColorID"] !== colorSelectionInputData["ColorID"])
+                    {
+                        analyzerData["GameSelection"]["ColorID"] = colorSelectionInputData["ColorID"];
+
+                        currentColorContainer.parent().find(".selected").removeClass("selected");
+
+                        $(this).addClass("selected");
+                    }
+                });
+            });
+
+            $("#game-selection > div:last-of-type button[name=\"confirm\"]").on("click", function ()
+            {
+                $(this).closest(".confirmation-actions").remove();
+
+                saveAnalyzerData([ "GameSelection" ]);
+
+                initializeDetermineCharacterSettings();
+            });
+        }
+    });
 });
